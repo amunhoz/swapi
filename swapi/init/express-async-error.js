c2ebecd81c76c800f136ba4531898555e5a9de49
amunhoz <ale.munhoz@gmail.com>
2017-07-23 15:59:19 -0300

    async errors

---
+++ swapi/init/express-async-error.js
@@ -0,0 +1,9 @@
+﻿
+module.exports = {
+    name: "express-async-error",
+    run: async function (app) {
+
+        require('express-async-errors');
+        console.log("(init) Express async errors loaded");
+    }
+}
