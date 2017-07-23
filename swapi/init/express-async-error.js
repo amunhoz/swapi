13b497435a097dfcd630068b218212731024b664
amunhoz <ale.munhoz@gmail.com>
2017-07-23 17:07:54 -0300

    melhor tratamento de erros asyncronos no express. Usando sysLog do sistema.

--- swapi/init/express-async-error.js
+++ swapi/init/express-async-error.js
@@ -2,8 +2,43 @@
 module.exports = {
     name: "express-async-error",
     run: async function (app) {
+        //from require('express-async-errors');
 
-        require('express-async-errors');
+        const Layer = require('express/lib/router/layer');
+
+        Object.defineProperty(Layer.prototype, "handle", {
+            enumerable: true,
+            get: function () { return this.__handle; },
+            set: function (fn) {
+                if (isAsync(fn)) {
+                    fn = wrap(fn);
+                }
+
+                this.__handle = fn;
+            }
+        });
+        function isAsync(fn) {
+            const type = Object.toString.call(fn.constructor);
+            return type.indexOf('AsyncFunction') !== -1;
+        };
+        function wrap(fn) {
+            return (req, res, next) => {
+                const routePromise = fn(req, res, next);
+                if (routePromise.catch) {
+                    routePromise.catch(err => {
+                        sysLog.error(err);
+                        res.status(500).send({ sucess: false, error: "internal server error" });
+                    });
+                    //routePromise.catch(err => next(err));
+                }
+            }
+        };
+
+        function treatError(err) {
+            sysLog.error(err);
+            res.status(500).send({ sucess: false, error: "internal server error" });
+        }
+
         console.log("(init) Express async errors loaded");
     }
 }
