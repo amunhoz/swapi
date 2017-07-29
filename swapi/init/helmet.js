3c76bbb379bad78cdc17dd66e224d76120385661
amunhoz <ale.munhoz@gmail.com>
2017-07-29 02:28:42 +0000

    helmet insted of lusca. disabled by default

---
+++ swapi/init/helmet.js
@@ -0,0 +1,12 @@
+﻿
+module.exports = {
+  name: "helmet",
+  run: async function (app) {
+
+		var helmet = require('helmet');
+		app.use(helmet());
+
+    console.log("(init) Helmet security loaded");
+	
+  }
+}
